899f9b33629466f89b1fd24dd3e8cf6f96c5ab56
Ben-git01 <benbakirbenjamin@orange.fr>
2023-07-31 12:25:52 +0200

    express 04

--- movieHandlers.js
+++ movieHandlers.js
@@ -84,7 +84,7 @@ const getMovieById = (req, res) => {
 const postMovie = (req, res) => {
 
   const { title, director, year, color, duration } = req.body;
-
+  console.log(req.body);
 
   database
 
@@ -112,9 +112,50 @@ const postMovie = (req, res) => {
 
 };
 
+const editMovie = (req, res) =>{
+  const id = parseInt(req.params.id);
+
+  const { title, director, year, color, duration } = req.body;
+
+  database
+
+    .query(
+
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+
+      [title, director, year, color, duration, id]
+
+    )
+
+    .then(([result]) => {
+
+      if (result.affectedRows === 0) {
+
+        res.status(404).send("Not Found");
+
+      } else {
+
+        res.sendStatus(204);
+
+      }
+
+    })
+
+    .catch((err) => {
+
+      console.error(err);
+
+      res.status(500).send("Error editing the movie");
+
+    });
+
+};
+
+
 
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
+  editMovie,
 };
