813510e84fc5f475101c515884c4cc922a005b1e
Ben-git01 <benbakirbenjamin@orange.fr>
2023-07-31 14:19:17 +0200

    express05

--- userHandler.js
+++ userHandler.js
@@ -124,10 +124,30 @@ const getUserById = (req, res) => {
       });
   
   };
+const deleteUser = (req , res ) => {
+  const id = parseInt(req.params.id);
+
+database
+  .query(
+    "delete from users where id = ? ",[id]
+  )
+  .then(([result]) =>{
+    if(result.affectedRows === 0 ){
+      res.status(404).sens("Not found");
+    }else{
+      res.sendStatus(204);
+    }
+  })
+  .catch((err)=>{
+    console.error(err);
+    res.status(500).send("Error deleting user");
+  })
+}
 
 module.exports = {
     getUsers,
     getUserById,
     postUser,
     editUser,
+    deleteUser
   };
